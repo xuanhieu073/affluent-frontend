79926ee7a824c03646fb248db9562a052f0dbc37
Nguyen Xuan Hieu <xuanhieu073@gmail.com>
2024-07-09 15:36:19 +0700

    add affluent-key-features

--- src/javascripts/revamp/params-handler.js
+++ src/javascripts/revamp/params-handler.js
@@ -1,64 +1,87 @@
 document.addEventListener("alpine:init", () => {
   Alpine.data("paramsHandler", () => ({
     el: null,
+    csvDataUrl: "",
     async init() {
       this.el = this.$root;
-      const url = "http://localhost:4999/files/params-map.csv";
-      const csvData = await this.readCsv(url);
-      for(const csvObj of csvData) {
-        const [fieldname, value] = Object.entries(csvObj)[0];
-        this.fillValue(fieldname, value)
-      }
 
-      const currentUrl = document.location.toString();
-      const params = new URL(currentUrl).searchParams;
-      for (const [fieldname, value] of params) {
-        this.fillValue(fieldname, value);
+      const allTextbox = this.el.querySelectorAll("input[data-param-key]");
+      for (const textbox of allTextbox) {
+        this.csvDataUrl = textbox.dataset.csvDataUrl;
+        const paramKey = textbox.dataset.paramKey;
+        const defaultValue = textbox.dataset.default;
+        const id = textbox.id;
+
+        const currentUrl = document.location.toString();
+        const params = new URL(currentUrl).searchParams;
+        const [, paramMap] = [...params].find(
+          ([fieldname]) => fieldname === paramKey
+        ) || [, null];
+        let value = paramMap || null;
+
+        if (this.csvDataUrl) {
+          try {
+            const csvData = await this.readCsv(this.csvDataUrl);
+            for (const csvObj of csvData) {
+              const [csvMap, mapValue] = Object.entries(csvObj)[0];
+              console.log("🚀 ~ init ~ [csvMap, mapValue]:", [csvMap, mapValue])
+              if (csvMap === paramMap) value = mapValue;
+            }
+          } catch {}
+        }
+        if (value !== null) this.fillValue(id, value, true);
+        else this.fillValue(id, defaultValue);
       }
     },
-    fillValue(fieldname, value) {
-      const input = this.$root.querySelector(
-        `[id="${fieldname}"]`
-      );
+
+    getSerializedData() {
+      return [{ name: "firstname", value: "firstname" }];
+    },
+
+    fillValue(fieldname, value, disabled = false) {
+      const input = this.el.querySelector(`[id="${fieldname}"]`);
       if (input) {
         switch (input.tagName) {
           case "SELECT":
-            if(value) {
-              const selectcontrol = input.closest('div');
-              const listItem = selectcontrol?.querySelector('.select-items');
-              const targetitem = [...listItem?.children || []].find(item => item.textContent === value);
-              if(targetitem) {
+            if (value) {
+              const selectcontrol = input.closest("div");
+              const listItem = selectcontrol?.querySelector(".select-items");
+              const targetitem = [...(listItem?.children || [])].find(
+                (item) => item.textContent === value
+              );
+              if (targetitem) {
                 targetitem.click();
-                selectcontrol.style.pointerEvents = 'none';
+                if (disabled) selectcontrol.style.pointerEvents = "none";
               }
             }
             break;
           case "TEXTAREA":
-            if(value) {
+            if (value) {
               input.value = value;
-              input.disabled = true;
+              if (disabled) input.disabled = true;
             }
             break;
           default:
             switch (input.type) {
               case "text":
-                if(value) {
+                if (value) {
                   input.value = value;
-                  input.disabled = true;
+                  if (disabled) input.disabled = true;
                 }
                 break;
               case "checkbox":
                 if (value && value !== "false") {
                   const label = this.el.querySelector(`[for="${fieldname}"]`);
                   label?.click();
-                  input.disabled = true;
+                  if (disabled) input.disabled = true;
                 }
                 break;
-              }
+            }
             break;
         }
       }
     },
+
     async readCsv(url) {
       const csvData = [];
       try {
@@ -71,7 +94,7 @@ document.addEventListener("alpine:init", () => {
         rows.forEach((row) => {
           if (row.trim()) {
             const cols = row
-              .match(/(".*?"|[^",;\s]+)(?=\s*[;,]|\s*$)/g)
+              .match(/(".*?"|[^",;\s]+|[^,]+)(?=\s*[;,]|\s*$)/g)
               .map((col) => col.replace(/["']/g, "").trim());
             const csvObj = { [cols[0]]: cols[1] };
             csvData.push(csvObj);
