62639d9d7668c72646d57bf82cc24c7a4691221a
Nguyen Xuan Hieu <xuanhieu073@gmail.com>
2024-07-23 16:55:09 +0700

    add hiw infographic side navigation

--- src/javascripts/affluent/affluent-hiw-infographic.js
+++ src/javascripts/affluent/affluent-hiw-infographic.js
@@ -1,6 +1,6 @@
 // import Swiper JS
-import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
 document.addEventListener("alpine:init", () => {
   Alpine.data("affluent_hiwinfographic", () => ({
@@ -8,9 +8,10 @@ document.addEventListener("alpine:init", () => {
     init() {
       this.swiper = new Swiper(this.$refs.swiper, {
         modules: [Navigation, Pagination],
-        slidesPerView: 'auto',
+        slidesPerView: "auto",
         spaceBetween: 8,
         centeredSlides: true,
+        slideToClickedSlide: true,
         pagination: {
           el: this.$refs.pagination,
           clickable: true,
@@ -20,10 +21,22 @@ document.addEventListener("alpine:init", () => {
         },
         breakpoints: {
           768: {
-            spaceBetween: 32,
-          }
-        }
-      })
+            spaceBetween: 0,
+          },
+        },
+      });
+      const { backButton, nextButton } = this.$root.dataset;
+      const backbutonStyle = `
+        .affluent-hiw-infographic_container .swiper-slide-next:hover { cursor: url(${nextButton}) 20 20, move; }
+        .affluent-hiw-infographic_container .swiper-slide-prev:hover { cursor: url(${backButton}) 15 15, move; }
+      `;
+      const style = document.createElement('style');
+      if (style.styleSheet) {
+        style.styleSheet.cssText = backbutonStyle;
+      } else {
+        style.appendChild(document.createTextNode(backbutonStyle));
+      }
+      document.getElementsByTagName('head')[0].appendChild(style);
     },
   }));
 });
